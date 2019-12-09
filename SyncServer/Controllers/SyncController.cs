c6619436353bd957147706407dc7833dc2be913f
Ferry Temaja Putra <ferry.temaja.putra@gmail.com>
2019-12-09 11:29:45 +0800

    Add client-server synchronisation projects.

---
+++ SyncServer/Controllers/SyncController.cs
@@ -0,0 +1,24 @@
+﻿using Dotmim.Sync.Web.Server;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace SyncServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SyncController : ControllerBase
+    {
+        private WebProxyServerProvider webProxyServer;
+
+        public SyncController(WebProxyServerProvider proxy)
+        {
+            webProxyServer = proxy;
+        }
+
+        [HttpPost]
+        public async Task Post()
+        {
+            await webProxyServer.HandleRequestAsync(HttpContext);
+        }
+    }
+}
